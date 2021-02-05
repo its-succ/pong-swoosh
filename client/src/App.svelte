b418ba60eb5f74e029cc3fa13e285f4f29d8d74d
sizuhiko <k-kishida@esm.co.jp>
2021-02-05 19:33:37 +0900

    add home of spa-router

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,28 +1,13 @@
-<style>
-main {
-  text-align: center;
-  padding: 1em;
-  max-width: 240px;
-  margin: 0 auto;
-}
-
-h1 {
-  color: #ff3e00;
-  text-transform: uppercase;
-  font-size: 4em;
-  font-weight: 100;
-}
-
-@media (min-width: 640px) {
-  main {
-    max-width: none;
-  }
-}
-</style>
-
 <script lang="ts">
+import Router from 'svelte-spa-router'
+import Home from './routes/home.svelte'
+
+const routes = {
+  '/': Home, // チャンネル作成
+//  '/contoller/:channel-slug': Controller, // コントローラ
+//  '/speaker/:channel-slug': Speaker, // スピーカー（リスナー）
+//  '*': NotFound,
+}
 </script>
 
-<main>
-  <h1>pong-swoosh</h1>
-</main>
+<Router {routes}/>
