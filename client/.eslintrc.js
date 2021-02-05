651d5c075a65df0e70c8e58b36d465a432a841c3
sizuhiko <k-kishida@esm.co.jp>
2021-02-05 19:27:08 +0900

    add svelte prettier plugin

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,34 +1,34 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname,
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/core-modules': ['svelte'],
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    'import/no-mutable-exports': 0,
+    'no-labels': 0,
+    'no-restricted-syntax': 0,
+  },
+  plugins: ['prettier', '@typescript-eslint', 'svelte3'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
     },
-    settings: {
-        'import/core-modules': ['svelte'],
-    },
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-    },
-    rules: {
-        'import/no-mutable-exports': 0,
-        'no-labels': 0,
-        'no-restricted-syntax': 0,
-    },
-    plugins: ['prettier', '@typescript-eslint', 'svelte3'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:eslint-comments/recommended',
-        'plugin:promise/recommended',
-        'prettier',
-        'prettier/@typescript-eslint',
-    ],
-    overrides: [
-        {
-            files: ['**/*.svelte'],
-            processor: 'svelte3/svelte3',
-        },
-    ],
+  ],
 };
--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,27 +1,28 @@
-<script lang="ts"></script>
+<style>
+main {
+  text-align: center;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+}
+
+h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+}
+
+@media (min-width: 640px) {
+  main {
+    max-width: none;
+  }
+}
+</style>
+
+<script lang="ts">
+</script>
 
 <main>
-	<h1>pong-swoosh</h1>
+  <h1>pong-swoosh</h1>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
