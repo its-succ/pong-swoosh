22a4b1ba7de02762b8effaf0a0d92d4100a062eb
sizuhiko <k-kishida@esm.co.jp>
2021-03-05 16:09:37 +0900

    add speaker route

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 import Router from 'svelte-spa-router'
 import Home from './routes/home.svelte'
+import Speacker from './routes/speaker.svelte'
 
 const routes = {
   '/': Home, // チャンネル作成
 //  '/contoller/:channel-slug': Controller, // コントローラ
-//  '/speaker/:channel-slug': Speaker, // スピーカー（リスナー）
+  '/speaker/:channelSlug': Speaker, // スピーカー（リスナー）
 //  '*': NotFound,
 }
 </script>
