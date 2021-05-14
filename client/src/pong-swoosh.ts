0e3f01623f8a2caad864df7cb68d82971ccb8fb4
sizuhiko <k-kishida@esm.co.jp>
2021-05-14 16:48:04 +0900

    add deploy settings

--- client/src/pong-swoosh.ts
+++ client/src/pong-swoosh.ts
@@ -1,1 +1,3 @@
-export const SERVER_URL = 'http://localhost:3000'; // 'https://pong-swoosh-server.herokuapp.com';
+// ローカルで起動する場合は、こちらを有効にする
+// export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = 'https://pong-swoosh-server.herokuapp.com';
