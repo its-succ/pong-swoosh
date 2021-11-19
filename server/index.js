9f104375e0dbbbb706d9f754ed6e27556e5ace9f
sizuhiko <k-kishida@esm.co.jp>
2021-11-19 14:29:40 +0900

    arrange volume

--- server/index.js
+++ server/index.js
@@ -183,7 +183,7 @@ io.on('connection', (socket) => {
           (s) => s.userrole === 'listener'
         ).length;
         debug('LISTENERS', listeners);
-        const volume = Math.sin((Math.PI * 90 * (count / listeners)) / 180) * 2;
+        const volume = Math.sin((Math.PI * 90 * (count / listeners)) / 180);
         const timestamp = DateTime.now().toFormat('yyyyMMddHHmmss');
         if (!pong.buffer) {
           const response = await fetch(pong.url);
