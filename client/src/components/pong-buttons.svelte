fc9de14faa1a1cf3e56889daedb9ebeedc740cd7
Miho Nakamoto <miho.nakamoto@esm.co.jp>
2022-09-02 18:32:53 +0900

    component化したつもり
    ほかの画面(pong-swoosh.svelte)への対応は別途

---
+++ client/src/components/pong-buttons.svelte
@@ -0,0 +1,54 @@
+<style>
+.pong-buttons {
+  padding: 2rem;
+}
+
+ul, li {
+  list-style: none;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin: 0;
+  padding: 0;
+}
+li {
+  padding: 10px 0;
+  align-items: center;
+}
+button {
+  height: 5rem;
+  width: 10rem;
+  position: relative;
+  margin: 1rem;
+}
+button img {
+  padding-left: 6px;
+  width: 2rem;
+  float: left;
+}
+button label {
+  font-weight: bold;
+  font-size: 0.8rem;
+  line-height: 2rem;
+}
+
+</style>
+<script lang="ts">
+    export let pongButtons = [];
+</script>
+<!-- TODO pong-swoosh.svelteのボタンも対応できるようにする -->
+<div class="pong-buttons">
+    <ul>
+      {#each pongButtons as button}
+        <li>
+          <button>
+            <img src="{button.icon}" alt="icon">
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label>{button.title}</label>
+          </button>
+        </li>
+      {/each}
+    </ul>
+  </div>
--- client/src/routes/home.svelte
+++ client/src/routes/home.svelte
@@ -99,44 +99,6 @@ form > mwc-button {
   display: inline-block;
 }
 
-/* TODO: ボタン表示のスタイルを修正する。
-   それかコンポーネント化して利用する。(pong-swoosh.svelteでも利用する
- */
-.default-buttons {
-  padding: 2rem;
-}
-
-ul, li {
-  list-style: none;
-}
-ul {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  margin: 0;
-  padding: 0;
-}
-li {
-  padding: 10px 0;
-  align-items: center;
-}
-button {
-  height: 5rem;
-  width: 10rem;
-  position: relative;
-  margin: 1rem;
-}
-button img {
-  padding-left: 6px;
-  width: 2rem;
-  float: left;
-}
-button label {
-  font-weight: bold;
-  font-size: 0.8rem;
-  line-height: 2rem;
-}
-
 .warning {
   background-color: lightyellow;
   margin: 5rem 50px 0 50px;
@@ -175,6 +137,7 @@ import { faUsers, faVolumeUp, faShareAlt } from '@fortawesome/free-solid-svg-ico
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from 'fontawesome-svelte';
 import copy from 'copy-to-clipboard';
+import PongButtons from './../components/pong-buttons.svelte';
 
 library.add(faUsers, faVolumeUp, faShareAlt, faCopy);
 
@@ -285,19 +248,7 @@ const unload = () => {
           <a href="{pongSwooshUrl}">{pongSwooshUrl}</a>
         </div>
         {#if defaultButtons}
-        <div class="default-buttons">
-          <ul>
-            {#each defaultButtons as button}
-              <li>
-                <button>
-                  <img src="{button.icon}" alt="icon">
-                  <!-- svelte-ignore a11y-label-has-associated-control -->
-                  <label>{button.title}</label>
-                </button>
-              </li>
-            {/each}
-          </ul>
-        </div>
+          <PongButtons pongButtons={defaultButtons}></PongButtons>
         {/if}
         <div class="warning">
           <strong>このページを離れると {channelName} が終了します。ご注意ください。</strong>
